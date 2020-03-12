f360f44443f6db58571ab78e0888983c715aa815
AhmedBennaji <a.bennaji96@gmail.com>
2020-03-12 05:16:22 -0600

    initial PA1

--- src/Client.cpp
+++ src/Client.cpp
@@ -16,29 +16,103 @@
 
 #include "common.hpp"
 #include "Client.hpp"
+#include <iostream>
+#include <fstream>
+#include <cereal/archives/json.hpp>
+#include <cereal/types/vector.hpp>
 
 Client::~Client() {
+
 }
 
-
-void Client::initialize(unsigned int player, unsigned int board_size){
+void Client::initialize(unsigned int player, unsigned int board_size) {
+    this->player = player;
+    this->board_size = board_size;
+    string fname = "player_" + to_string(player) + ".action_board.json";
+    // 10 x 10  2D vector
+    std::vector<std::vector<int>> board(board_size, vector<int>(board_size));
+    std::ofstream file(fname);
+    // create an output archive
+    cereal::JSONOutputArchive archive(file);
+    archive( CEREAL_NVP(board));
+    initialized = true;
 }
-
 
 void Client::fire(unsigned int x, unsigned int y) {
+string fname = "player_" + to_string(this->player) + ".shot.json";
+ofstream file1(fname);
+cereal::JSONOutputArchive read_archive1(file1);
+read_archive1(CEREAL_NVP(x), CEREAL_NVP(y));
+file1.close();
 }
+
 
 
 bool Client::result_available() {
+    /* try to open file to read */
+    ifstream player1 ("player_"+ to_string(player)+ ".result.json");
+    if(player1) {
+        cout<<"file exists";
+    } else {
+        cout<<"file doesn't exist";
+   }
 }
 
 
 int Client::get_result() {
-}
+    string fname = ("player_" + to_string(player)+".result.json");
+    int result;
+    ifstream file3(fname);
+    cereal::JSONInputArchive read_archive(file3);
+        read_archive(result);
+
+        if(result == HIT){
+            return HIT;
+        }
+        if (result == MISS){
+            return MISS;
+        }
+        if(result == OUT_OF_BOUNDS){
+            return OUT_OF_BOUNDS;
+        }
+        if(result == 999) {
+            throw ClientException("bad result");
+        }
+
+
+
+                    std:: remove("player_1.result.json");
+
+     }
+
+
+
+
+
 
 
 
 void Client::update_action_board(int result, unsigned int x, unsigned int y) {
+
+    string fname = ("player_" + to_string(player)+".result.json");
+
+
+    ifstream file3(fname);
+    cereal::JSONInputArchive read_archive(file3);
+    read_archive(CEREAL_NVP(x),CEREAL_NVP(y));
+
+     x = 1;
+     y = 1;
+
+
+
+    ofstream file7( "player_" + to_string(player) + ".action_board.json");
+    std::vector<std::vector<int>> board1(board_size, vector<int>(board_size));
+    cereal::JSONOutputArchive outputArchive(file7);
+
+    outputArchive(CEREAL_NVP(result));
+
+
 }
 
 
